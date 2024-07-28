7b650fc8059d186f388a084d4d8626ec86df2e0a
Eduardo Petrilli <petrillieduardo4@gmail.com>
2024-07-28 19:39:47 -0300

    Ajustado carrosel

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { ProjectsService } from './projects.service';
+import { Projects } from './projects.model';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CarouselModule, ButtonModule, TagModule ],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
+  host: { ngSkipHydration: 'true' }
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: Projects[] = [];
 
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 2,
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
+
+  constructor(private productService: ProjectsService) {}
+
+  ngOnInit() {
+    this.projects = this.productService.obterProjetos();
+  }
 }
---
+++ src/app/components/projects/projects.model.ts
@@ -0,0 +1,10 @@
+
+export interface Projects {
+  id?: string;
+  tag?: string;
+  empresa?: string;
+  descricao?: string;
+  cargo?: string;
+  siteEmpresa?: string;
+  img?: string;
+}
